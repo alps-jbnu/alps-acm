b351376a74961850c5a0366e4166c4e24bb13289
Jaewon Song <sjw0687@gmail.com>
2015-05-14 08:54:23 +0900

    Team registeration

--- client/templates/main.js
+++ client/templates/main.js
@@ -13,19 +13,85 @@ Template.main.onRendered(function() {
     renderNavbar();
     $(window).scroll(renderNavbar);
     $(window).resize(renderNavbar);
+
+    document.title = '전북대학교 컴퓨터공학부 프로그래밍 경진대회';
 });
 
 Template.main.events({
-    'click .goto' : function(e) {
+    'click .goto': function(e) {
         e.preventDefault();
+
         $("html, body").animate({
             scrollTop: $($.attr(e.target, "href")).offset().top - 120
         }, "slow");
     },
-    'click .goto-top' : function(e) {
+    'click .goto-top': function(e) {
         e.preventDefault();
+
         $("html, body").animate({
             scrollTop: 0
         }, "slow");
+    },
+    'click .under-construction': function(e) {
+        e.preventDefault();
+
+        alert('죄송합니다. 아직 준비 중입니다.');
+    },
+    'submit #team-apply-form': function(e) {
+        e.preventDefault();
+
+        var team = {
+            name: $(e.target).find('[name=name]').val(),
+            leaderName: $(e.target).find('[name=leaderName]').val(),
+            leaderStudentNumber: $(e.target).find('[name=leaderStudentNumber]').val(),
+            leaderPhoneNumber: $(e.target).find('[name=leaderPhoneNumber]').val(),
+            memberOneName: $(e.target).find('[name=memberOneName]').val(),
+            memberOneStudentNumber: $(e.target).find('[name=memberOneStudentNumber]').val(),
+            memberTwoName: $(e.target).find('[name=memberTwoName]').val(),
+            memberTwoStudentNumber: $(e.target).find('[name=memberTwoStudentNumber]').val(),
+        };
+
+        Meteor.call('teamInsert', team, function(err, res) {
+            if (err && err.error === 'teamInsert failed') {
+                alert(err.reason);
+            } else if (err) {
+                alert('등록에 실패했습니다. 다시 시도해주세요.');
+            } else {
+                $(e.target).find('[name=name]').val('');
+                $(e.target).find('[name=leaderName]').val('');
+                $(e.target).find('[name=leaderStudentNumber]').val('');
+                $(e.target).find('[name=leaderPhoneNumber]').val('');
+                $(e.target).find('[name=memberOneName]').val('');
+                $(e.target).find('[name=memberOneStudentNumber]').val('');
+                $(e.target).find('[name=memberTwoName]').val('');
+                $(e.target).find('[name=memberTwoStudentNumber]').val('');
+                alert('접수되었습니다. 감사합니다.')
+            }
+        });
+    }
+});
+
+Template.main.helpers({
+    teams: function() {
+        return Teams.find();
+    },
+    blankTeams: function() {
+        var cnt = Teams.find().count();
+        var minCnt = 6;
+        if (cnt < minCnt) {
+            var ret = [];
+            var insertCnt = minCnt - cnt;
+            for (var i = 0; i < insertCnt; i++) {
+                ret.push({
+                    name: '',
+                    leaderName: '',
+                    memberOneName: '',
+                    memberTwoName: ''
+                });
+            }
+            return ret;
+        } else {
+            return null;
+        }
     }
 });
